c301fb490c16cf4792e091fa0196aa76ad487e92
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-04-14 15:57:43 +0200

    completato snack-4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,8 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+const risultato = posts.map((persona) => {
+    return persona.author;
+});
+
+console.log(risultato);
