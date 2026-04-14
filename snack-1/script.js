b4fbb3092be582e521cd1fd4a2ecbc3a6ea68fcd
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-04-14 15:42:37 +0200

    completato snack-1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,8 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+const risultato = names.map((nome) => {
+    return `'${nome}'`;
+}).join(', ');
+
+console.log(risultato);
