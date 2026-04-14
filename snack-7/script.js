cd5d51a76d54c460a75919874b87ea3f1654a71c
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-04-14 16:18:52 +0200

    completato snack-7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,8 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+const risultato = students.find((studente) => {
+  return studente.id === 2;
+})
+
+console.log(risultato);
