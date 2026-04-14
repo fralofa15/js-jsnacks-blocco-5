93c9f7bdab3491821d2af81d37127da48f4cd9dc
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-04-14 16:00:52 +0200

    completato snack-5

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,8 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+const risultato = nums.filter((numeri) => {
+    return numeri % 2 === 0;
+})
+
+console.log(risultato);
